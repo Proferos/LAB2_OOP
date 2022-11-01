ac9fdef069f06897c7ce1a2e0b16ecdad4c43c5f
Proferos <ketchypskoloy@gmail.com>
2022-11-01 09:33:59 +0100

    changed basic method create

--- GameAccount/GameAccount/Program.cs
+++ GameAccount/GameAccount/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace GameAccount
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameAccount
+
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            GameAccount.Create("nagibator228");
+            GameAccount.Create("ubivator337");
+
+            Game.Play("nagibator228", "ubivator337", 30);
+            Game.Play("nagibator228", "ubivator337", 30);
+            Game.Play("nagibator228", "ubivator337", 30);
+            Game.Play("nagibator228", "ubivator337", 30);
+            Game.Play("nagibator228", "ubivator337", 1000);
+            Game.Play("nagibator228", "ubivator337", 0);
+            Game.Play("nagibator228", "ubivator337", -30);
+            GameAccount.GetStats("nagibator228");
+            GameAccount.GetStats("ubivator337");
+
+
         }
+
     }
+
+
 }
